88bc3e198148dcf20d04259c463146d6c5687284
jfear <jfear@pairwise.com>
2023-06-23 21:21:55 +0000

    update to use mambaforge

--- conda/conf.d/conda.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-if test -e $HOME/miniconda3/bin/conda
-    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
--- conda/install.fish
+++ conda/install.fish
@@ -1,13 +1,26 @@
 #!/usr/bin/env fish
 
-if ! test -d $HOME/miniconda3
-    read -l -P "Install miniconda? [Y/n] " confirm
+if ! test -d $HOME/mambaforge
+    read -l -P "Install mambaforge? [Y/n] " confirm
     switch $confirm
         case '' Y y
-            bash -c "curl -SsLo mini.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-                and bash mini.sh -b
+            read -l -P "Install location? [~/mambaforge] " prefix
+            switch $prefix
+                case ''
+                    set prefix $HOME/mambaforge
+            end
+
+
+            bash -c "curl -SsLo mini.sh https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
+                and bash mini.sh -b -p $prefix
                 and rm ./mini.sh
-                and eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-                and conda install -c conda-forge mamba condax -y
+                and eval $prefix/bin/conda "shell.fish" "hook" $argv | source
+                and echo "\
+                if test -e $prefix/bin/conda
+                    eval $prefix/bin/conda \"shell.fish\" \"hook\" \$argv | source
+                end
+                " | fish_indent > $HOME/.config/fish/conf.d/mamba.fish
+                and mamba update --all -y
+                and mamba install -c conda-forge condax invoke black ruff -y
     end
 end
